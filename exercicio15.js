c25cf662c00306d686debb9e41e7e33e43ca734b
hector <hector.guedesm2006@gmail.com>
2024-03-13 17:24:36 -0300

    recriei o exercicio15

--- exercicio15.js
+++ exercicio15.js
@@ -1,14 +1,19 @@
+console.log('===================CARDÁPIO==================')
 let prompt = require('prompt-sync')();
 let pos;
-pos = parseInt(prompt('Digite a posição: '))
+console.log('1 - Pizza com Coca Cola, 2 - Hamburguer e Fritas + Guaraná, 3 - Cachorro quente + Fanta Uva')
+pos = parseInt(prompt('Digite uma combinação: '))
 switch(pos) {
     case 1: 
-        console.log('Primeiro lugar');
+        console.log('Você Escolheu o combo1: ');
         break;
     case 2: 
-        console.log('Primeiro lugar');
+        console.log('Você escolheu o combo2: ');
         break;
     case 3: 
-        console.log('Primeiro lugar');
+        console.log('Você escolheu o combo3:');
+        break;
+    default:
         break;
 }
+console.log('=============================================')
