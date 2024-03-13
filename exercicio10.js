3ddb5a90652789f92e0a997babbfe1c2e4d6d37b
hector <hector.guedesm2006@gmail.com>
2024-03-13 16:12:59 -0300

    preciso terminar o exercicio10

---
+++ exercicio10.js
@@ -0,0 +1,14 @@
+let prompt = require ('prompt-sync')();
+n1 = parseInt(prompt('digite um valor: '));
+if (n1 >= 1) {
+    console.log('valor e positivo', n1); 
+} 
+
+else if (n1 = 0 ){
+    console.log('Você digitou zero');
+}    
+
+else {
+    console.log('o valor e negativo', n1);
+}
+    
