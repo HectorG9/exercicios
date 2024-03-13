b41cd0f9af7e421d17b216b592877d4f87688922
hector <hector.guedesm2006@gmail.com>
2024-03-13 15:03:28 -0300

    terminei o exercicio5 e melhorei ele

--- exercicio5.js
+++ exercicio5.js
@@ -1,3 +1,6 @@
 let prompt = require ('prompt-sync')();
 produto = parseFloat(prompt('Qual o valor do Produto? R$'))
-reajuste = produto
+reajuste = (produto * 33.33 / 100)
+Nproduto = reajuste + produto
+console.log('O Valor do produto é R$', produto + ' O reajuste é R$', reajuste)
+console.log('O novo valor reajustado R$', Nproduto)
