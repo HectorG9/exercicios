7a088e18bd895c6a2ccc91128511fc06d4be246e
hector <hector.guedesm2006@gmail.com>
2024-03-13 14:31:10 -0300

    modifiquei o exercicio2

--- exercicio2.js
+++ exercicio2.js
@@ -1,4 +1,4 @@
-let prompt = require("prompt-sync")();
+let prompt = require ('prompt-sync')();
 dolar = parseFloat(prompt('digite o valor que ira converter $ '))
 reais = (dolar * 4.97)
 console.log('Você tem U$', dolar + 'E R$', reais)
