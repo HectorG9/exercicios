92c6588e039fdf052ac109132ac12d1dbb109c40
hector <hector.guedesm2006@gmail.com>
2024-03-13 17:09:08 -0300

    resolvi o exercicio15

---
+++ exercicio15.js
@@ -0,0 +1,14 @@
+let prompt = require('prompt-sync')();
+let pos;
+pos = parseInt(prompt('Digite a posição: '))
+switch(pos) {
+    case 1: 
+        console.log('Primeiro lugar');
+        break;
+    case 2: 
+        console.log('Primeiro lugar');
+        break;
+    case 3: 
+        console.log('Primeiro lugar');
+        break;
+}
