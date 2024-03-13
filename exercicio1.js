1dce76493865565371c338a181f62a035141448e
hector <hector.guedesm2006@gmail.com>
2024-03-13 14:10:56 -0300

    Resolvi o primeiro exercício e farei o segundo

---
+++ exercicio1.js
@@ -0,0 +1,8 @@
+
+console.log('========================Média Aritmética!====================')
+let n1 = 8
+let n2 = 6
+let n3 = 6
+media = (n1+n2+n3)/3
+console.log('A média aritmética é: ', media)
+console.log('========================================================');
