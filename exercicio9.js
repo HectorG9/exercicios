a60fdf38d1e920fbd2fdee6af974e5a43787d108
hector <hector.guedesm2006@gmail.com>
2024-03-13 15:57:51 -0300

    resolvi o exercicio9 e criei o 10

---
+++ exercicio9.js
@@ -0,0 +1,10 @@
+let prompt = require ('prompt-sync')();
+
+n1 = parseInt(prompt('digite um valor: '))
+if (n1 >= 0){
+    console.log('valor e positivo',n1)
+} 
+else{
+    console.log('o valor e negativo',n1)
+
+}
