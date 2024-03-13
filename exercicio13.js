d823ce0d3dc1c8897a213949a143fcd86a2c43fb
hector <hector.guedesm2006@gmail.com>
2024-03-13 16:36:46 -0300

    com muito custo resolvemos o exercicio13

---
+++ exercicio13.js
@@ -0,0 +1,9 @@
+let prompt = require ('prompt-sync')();
+n1 = parseInt(prompt('digite um numero: '));
+p_i = (n1 % 2) 
+if(p_i == 0) {
+    console.log('O seu número é par')
+}
+else {
+    console.log('O número é impar')
+}
